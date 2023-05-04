f72e72bfa36077ce8b020bd5ff34697ff6a5de5d
Sean <SWaltrs23@gmail.com>
2023-05-03 20:29:57 -0400

    Add functionality for opening/closing form, and changing button color

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const formButton = document.querySelector('.form-button');
+const formToggle = document.querySelector('.book-form');
 let myLibrary = [];
+
+formButton.addEventListener('click', () => {
+    formButton.classList.toggle('green');
+    formButton.classList.toggle('red');
+    if (formButton.textContent === '+') {
+        formButton.textContent = '-';
+    } else {
+        formButton.textContent = '+';
+    }
+
+    formToggle.classList.toggle('close');
+    formToggle.classList.toggle('open');
+});
 
 function Book(title, author, pages, read) {
     this.title = title;
