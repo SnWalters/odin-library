0dc80dbd1f4c4b3e49bc31f5a80332a09bac10bd
Sean <SWaltrs23@gmail.com>
2023-05-05 23:45:29 -0400

    Add function to add create Book object and push to myLibrary array

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const checkBox = document.querySelector('#read');
+const inputs = document.querySelectorAll('.text');
 const formButton = document.querySelector('.form-button');
 const formToggle = document.querySelector('.book-form');
+const addBook = document.querySelector('.btn');
 let myLibrary = [];
 
 formButton.addEventListener('click', () => {
@@ -16,15 +18,18 @@ formButton.addEventListener('click', () => {
     formToggle.classList.toggle('open');
 });
 
-checkBox.addEventListener('click', () => {
-    if (checkBox.checked) {
-        console.log('checked')
-    }
-});
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
+
+addBook.addEventListener('click', () => {
+    myLibrary.push(new Book(inputs[0].value, inputs[1].value,
+        inputs[2].value, checkBox.checked));
+
+    myLibrary.forEach(entry => {
+        console.log(entry.title, entry.author, entry.pages, entry.read);
+    });
+});
