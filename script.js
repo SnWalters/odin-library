a04dae33fac7f03480fbf402dd1b2cc5ba8745fd
Sean <SWaltrs23@gmail.com>
2023-05-09 08:30:08 -0400

    Create function to display library

--- script.js
+++ script.js
@@ -3,7 +3,46 @@ const inputs = document.querySelectorAll('.text');
 const formButton = document.querySelector('.form-button');
 const formToggle = document.querySelector('.book-form');
 const addBook = document.querySelector('.btn');
+const main = document.querySelector('main');
 let myLibrary = [];
+
+const displayLibrary = () => {
+    main.textContent = "";
+
+    myLibrary.forEach((entry) => {
+        const entryContainer = document.createElement('div');
+        entryContainer.classList = 'library-entry';
+
+        let para = document.createElement('p');
+        para.classList = 'title';
+        para.textContent = `Title: ${entry.title}`;
+        entryContainer.append(para);
+
+        para = document.createElement('p');
+        para.classList = 'author';
+        para.textContent = `Author: ${entry.author}`;
+        entryContainer.append(para);
+
+        para = document.createElement('p');
+        para.classList = 'pages';
+        para.textContent = `Total Pages: ${entry.pages}`;
+        entryContainer.append(para);
+
+        if (entry.read) {
+            para = document.createElement('p');
+            para.classList = 'have-read';
+            para.textContent = 'Have Read';
+            entryContainer.append(para);
+        } else {
+            para = document.createElement('p');
+            para.classList = 'not-read';
+            para.textContent = 'Not Yet Read';
+            entryContainer.append(para);
+        };
+
+        main.append(entryContainer);
+    });
+}
 
 formButton.addEventListener('click', () => {
     formButton.classList.toggle('green');
@@ -29,7 +68,5 @@ addBook.addEventListener('click', () => {
     myLibrary.push(new Book(inputs[0].value, inputs[1].value,
         inputs[2].value, checkBox.checked));
 
-    myLibrary.forEach(entry => {
-        console.log(entry.title, entry.author, entry.pages, entry.read);
-    });
+    displayLibrary();
 });
